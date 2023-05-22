b89e0ee6d93d9b20cf2c3c31bd428fb879606fea
DiaTime <benoz@me.com>
2023-05-22 21:35:40 +0300

    working

--- run_app.py
+++ run_app.py
@@ -1,6 +1,16 @@
 import os
 import subprocess
 import sys
+import venv
+
+def create_and_activate_venv():
+    if not os.path.exists('env'):
+        venv.create('env', with_pip=True)
+    if sys.platform == 'win32':
+        activate_script = 'env\\Scripts\\activate.bat'
+    else:
+        activate_script = 'source env/bin/activate'
+    os.system(activate_script)
 
 def install_requirements():
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
@@ -9,5 +19,6 @@ def run_app():
     os.system("streamlit run app.py")
 
 if __name__ == "__main__":
+    create_and_activate_venv()
     install_requirements()
     run_app()
